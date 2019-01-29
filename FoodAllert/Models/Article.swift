772df57d49d92cace07f86d2498be88cba15c5a5
José Gil Ramírez Sánchez <josegil_ramirez@sps.com.mx>
2019-01-28 19:21:22 -0600

    struct for article model added

---
+++ FoodAllert/Models/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  FoodAllert
+//
+//  Created by José Gil Ramírez S on 1/28/19.
+//  Copyright © 2019 José Gil Ramírez S. All rights reserved.
+//
+
+import Foundation
+
+struct Article: Codable {
+    let title: String
+    let abstract: String
+    let imageURL: String
+    let articleURL: String
+}
